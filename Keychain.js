c5177710b1e0eff38a81d495f73c27f2bc6e0a2f
Brenton House <brenton.house@gmail.com>
2019-06-28 08:49:34 -0500

    adding helper classes for keychain

---
+++ Keychain.js
@@ -0,0 +1,49 @@
+const identity = require('ti.identity');
+const KeychainItemNotFound = require('../KeychainItemNotFound');
+const teamId = Ti.App.Properties.getString('AppTeamId', Alloy.CFG.AppTeamId || 'TEAMID');
+
+export default class Keychain {
+	constructor(identifier) {
+		this.identifier = identifier;
+		this.keychainItem = identity.createKeychainItem({
+			identifier:  identifier,
+			accessGroup: `${teamId}.${Ti.App.getId()}`,
+		});
+	}
+	load() {
+		// eslint-disable-next-line promise/avoid-new
+		return new Promise((resolve, reject) => {
+			this.keychainItem.fetchExistence(e => {
+				if (e.exists) {
+					this.keychainItem.addEventListener('read', e => {
+						if (!e.success) {
+							reject(null);
+						}
+						// Return the value
+						resolve(e.value);
+					});
+					this.keychainItem.read();
+				} else {
+					reject(new KeychainItemNotFound('Keychain does not exist.'));
+				}
+			});
+		});
+	}
+
+	save(value) {
+		this.keychainItem.fetchExistence(e => {
+			if (e.exists) {
+				this.keychainItem.update(value);
+			} else {
+				this.keychainItem.save(value);
+			}
+		});
+	}
+
+	reset() {
+		this.keychainItem.reset();
+	}
+
+}
+
+module.exports = Keychain;
---
+++ KeychainItemNotFound.js
@@ -0,0 +1,18 @@
+export default class KeychainItemNotFound extends Error {
+	constructor(message) {
+	  super(message);
+	  this.name = 'KeychainItemNotFound';
+	}
+
+	toJSON() {
+
+		return {
+		  error: {
+				name:       this.name,
+				message:    this.message,
+				stacktrace: this.stack.split('\n'),
+		  },
+		};
+	}
+}
+module.exports = KeychainItemNotFound;
